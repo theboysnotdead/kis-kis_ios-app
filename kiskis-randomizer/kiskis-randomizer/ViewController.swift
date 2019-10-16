6e6fbe1803c495ab69b0147a6c74f5bda1a89ea0
v.onishchenko@andersenlab.com <>
2019-10-17 00:53:50 +0300

    add first screen configurations

--- kiskis-randomizer/kiskis-randomizer/ViewController.swift
+++ kiskis-randomizer/kiskis-randomizer/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+private enum KisKisMember {
+    case sonya, alyona
+    
+    var value: String {
+        switch self {
+        case .sonya: return "sonya"
+        case .alyona: return "alyona"
+        }
     }
-
-
+    
+    var image: UIImage {
+        return UIImage(named: value) ?? UIImage()
+    }
 }
 
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var imageBackground: UIImageView!
+    @IBOutlet private weak var segmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageBackground.image = KisKisMember.sonya.image
+    }
+    
+    @IBAction func kiskisMemberChanged(_ sender: Any) {
+        imageBackground.image = segmentControl.selectedSegmentIndex == 0
+            ? KisKisMember.sonya.image
+            : KisKisMember.alyona.image
+    }
+}
