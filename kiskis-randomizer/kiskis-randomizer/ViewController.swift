0981bdc07cdebeddce25ccdc18b52ac961940922
v.onishchenko@andersenlab.com <>
2019-10-17 00:23:44 +0300

    initial commit

---
+++ kiskis-randomizer/kiskis-randomizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kiskis-randomizer
+//
+//  Created by Vsevolod Onishchenko on 17/10/2019.
+//  Copyright © 2019 theboysnotdead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
